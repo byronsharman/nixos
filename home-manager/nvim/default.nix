7935c3b8ce5499b332d4c491f5da98376862fc7d
byronsharman <byron.n.sharman@gmail.com>
2026-02-28 02:35:29 -0700

    nvim: search symbols in telescope with leader s

--- home-manager/nvim/default.nix
+++ home-manager/nvim/default.nix
@@ -80,6 +80,7 @@
           vim.keymap.set('n', '<leader>f', builtin.find_files, {})
           vim.keymap.set('n', '<leader>g', builtin.git_status, {})
           vim.keymap.set('n', '<leader>l', builtin.live_grep, {})
+          vim.keymap.set('n', '<leader>s', builtin.lsp_document_symbols, {})
 
           require('telescope').load_extension('fzf')
           require('telescope').load_extension('ui-select')
